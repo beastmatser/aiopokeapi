95d0f6094098d2f2b41c8f415caef408c509c62d
beastmatser <79206232+beastmatser@users.noreply.github.com>
2022-01-19 09:18:14 +0100

    :rewind: Revert ":bookmark: 0.1.0"

--- aiopoke/__init__.py
+++ aiopoke/__init__.py
@@ -2,7 +2,7 @@ __title__ = "aiopoke"
 __author__ = "beastmatser"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021-present beastmatser"
-__version__ = "0.1.0"
+__version__ = "0.0.8a"
 
 from typing import NamedTuple
 
@@ -83,7 +83,7 @@ class VersionInfo(NamedTuple):
         return f"{self.major}.{self.minor}.{self.micro}-{self.releaselevel}"
 
 
-version_info: VersionInfo = VersionInfo(0, 1, 0, "final")
+version_info: VersionInfo = VersionInfo(0, 0, 8, "alpha")
 
 
 __all__ = (
