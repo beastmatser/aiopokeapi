d97f282f2ad41a58e16ff23997575ae2d0a3ec97
beastmatser <79206232+beastmatser@users.noreply.github.com>
2022-04-08 23:03:29 +0200

    :sparkles: Add `__main__.py`

---
+++ aiopoke/__main__.py
@@ -0,0 +1,19 @@
+from aiopoke import version_info
+
+
+def main():
+    print(
+        f"""
+\033[1;32m
+ .d8b.  d888888b  .d88b.  d8888b.  .d88b.  db   dD d88888b
+d8' `8b   `88'   .8P  Y8. 88  `8D .8P  Y8. 88 ,8P' 88'
+88ooo88    88    88    88 88oodD' 88    88 88,8P   88ooooo
+88~~~88    88    88    88 88~~~   88    88 88`8b   88~~~~~
+88   88   .88.   `8b  d8' 88      `8b  d8' 88 `88. 88.
+YP   YP Y888888P  `Y88P'  88       `Y88P'  YP   YD Y88888P\033[0m\t\033[1;34mVersion: {version_info}\033[0m
+"""
+    )
+
+
+if __name__ == "__main__":
+    main()
