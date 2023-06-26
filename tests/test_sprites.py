5af9aa8ed17c34a4f2799f91d4cf57d2d7a45094
beastmatser <79206232+beastmatser@users.noreply.github.com>
2023-06-27 00:16:02 +0200

    :white_check_mark: Add tests for sprites

---
+++ tests/test_sprites.py
@@ -0,0 +1,14 @@
+import os
+import pytest
+
+import aiopoke
+
+
+@pytest.mark.asyncio
+async def test_sprites(client: aiopoke.AiopokeClient):
+    pokemon = await client.get_pokemon(25)
+    sprites = pokemon.sprites
+
+    await sprites.back_default.save()
+    assert "back_25.png" in os.listdir()
+    assert sprites.back_default.client == client
